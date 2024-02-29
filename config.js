5dc2de7fcb36b3b8b8d3c31062b380852d230ce5
Medhouasly <152456900+Medhouasly@users.noreply.github.com>
2024-03-01 00:14:05 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  bot: {
+    owners: ["965569783473008640" , "1091666626627252224"],// owners id
+    botID: "1106230290977914920",// bot id
+    GuildId: "1068938684554018836", // Your Server Id
+    ClientId: "1106230290977914920", // Clietn Id
+    serverinvte: "https://discord.gg/WRbSssdZ", // Server invite url
+    clientSECRET: "GMZxhgFFw8LjeYE4J5eHd6NDc60F8q8_", // Client Secret
+    callbackURL: "https://bletoyt-bot-verification.bletoyt.repl.co/login", // Call Back Url
+    inviteBotUrl: "https://discord.com/api/oauth2/authorize?client_id=1103382321450733648&permissions=8&redirect_uri=https%3A%2F%2Fbletoyt-bot-verification.bletoyt.repl.co%2Flogin&scope=bot",
+    TOKEN: (process.env.token) // Bot Token
+  },
+  website: {
+    PORT: "3004",//website port
+  }
+}
